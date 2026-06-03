09319c412a4aeb13143650a87058269f29316b2e
Sumith Kumar <sumithkumar.dev@gmail.com>
2026-06-03 08:48:23 +0530

    Add evetns and final cafe website updates

--- src/data/events.ts
+++ src/data/events.ts
@@ -1,11 +1,13 @@
-import { galleryImages } from "./gallery";
+import event1 from "../assets/event-01.jpg";
+import event2 from "../assets/event-02.jpg";
+import event3 from "../assets/event-03.jpg";
 
 export const events = [
   {
     title: "Acoustic Night",
     date: "14 June 2026",
     time: "7:00 PM",
-    image: galleryImages[4],
+    image: event1,
     description:
       "An evening of live acoustic music featuring local artists and relaxed café vibes.",
   },
@@ -13,7 +15,7 @@ export const events = [
     title: "Open Mic Friday",
     date: "21 June 2026",
     time: "6:30 PM",
-    image: galleryImages[6],
+    image: event2,
     description:
       "Poetry, comedy, music and storytelling. Step up to the mic or simply enjoy the show.",
   },
@@ -21,7 +23,7 @@ export const events = [
     title: "Coffee Brewing Workshop",
     date: "28 June 2026",
     time: "5:00 PM",
-    image: galleryImages[8],
+    image: event3,
     description:
       "Learn brewing techniques, bean selection and café secrets from our baristas.",
   },
